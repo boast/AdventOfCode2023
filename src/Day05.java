a63800bda9f252fa5c51906c317d6fc9d236f0fa
Dave Thalmann <david.thalmann@gmail.com>
2023-12-05 23:35:43 +0100

    Day05
    Oh, what a day! Part 1 was supper simple to grasp and one could easily see, that it's better to calculate with offsets and ranges instead of actually iterating those very large values.
    
    But part 2 was a beast on its own. I realized the core concept pretty fast and I found the different cases early on (range is outside  the map, inside, partially before and partially after). But then the bugs kept on creeping in: first I missed, that you should not modify already offset ranges multiple times. After that I doubted my (actually decent) strategy to use left-hand inclusive, right-hand exclusive intervals (start/end respectively), but the actual error was just a wrong map.src() instead of a map.end(). I even started looking at reddit solutions (that's new for day 05...) but found out, that I was actually on the correct path and after some debugging, I eventually came to the current solution.

---
+++ src/Day05.java
@@ -0,0 +1,124 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day05 {
+    public static void main(final String[] args) throws IOException {
+        final var lines = Files.readAllLines(Paths.get("resources/day05.txt"));
+        
+        System.out.println(part1(lines));
+        System.out.println(part2(lines));
+    }
+    
+    static long part1(final List<String> lines) {
+        final var seeds   = Arrays.stream(lines.get(0).split(": ")[1].split(" ")).map(Long::parseLong).toList();
+        final var almanac = parseAlmanac(lines);
+        
+        return seeds.stream().map((seed) -> {
+            var currentValue = seed;
+            for (final var listMap : almanac) {
+                for (final var map : listMap) {
+                    if (currentValue >= map.src() && currentValue < map.end()) {
+                        currentValue += map.offset();
+                        break;
+                    }
+                }
+            }
+            
+            return currentValue;
+        }).min(Long::compare).orElseThrow();
+    }
+    
+    static long part2(final List<String> lines) {
+        final var seeds         = Arrays.stream(lines.get(0).split(": ")[1].split(" ")).map(Long::parseLong).toList();
+        final var initialRanges = new ArrayList<Range>();
+        
+        for (int i = 0; i < seeds.size(); i += 2) {
+            initialRanges.add(new Range(seeds.get(i), seeds.get(i) + seeds.get(i + 1)));
+        }
+        
+        final var almanac = parseAlmanac(lines);
+        var       min     = Long.MAX_VALUE;
+        
+        for (final var initialRange : initialRanges) {
+            var currentRanges = new ArrayList<Range>();
+            currentRanges.add(initialRange);
+            for (final var listMap : almanac) {
+                // We must not split up an offset range, so we need to keep track of them separately
+                final var offsetRanges = new ArrayList<Range>();
+                
+                for (final var map : listMap) {
+                    final var newRanges = new ArrayList<Range>();
+                    
+                    for (final var range : currentRanges) {
+                        // If the range is completely before or after the map, we can just add it to the next iteration
+                        if (range.start() >= map.end() || range.end() <= map.src()) {
+                            newRanges.add(range);
+                            continue;
+                        }
+                        
+                        // Range starts before the map, so we split away the part before the map
+                        if (range.start() < map.src()) {
+                            newRanges.add(new Range(range.start(), map.src()));
+                        }
+                        
+                        // Range is inside the map -> offset it
+                        final var innerStart = Math.max(range.start(), map.src());
+                        final var innerEnd   = Math.min(range.end(), map.end());
+                        if (innerStart < innerEnd) {
+                            offsetRanges.add(new Range(innerStart + map.offset(), innerEnd + map.offset()));
+                        }
+                        
+                        // Range ends after the map, so we split away the part after the map
+                        if (map.end() < range.end()) {
+                            newRanges.add(new Range(map.end(), range.end()));
+                        }
+                    }
+                    
+                    currentRanges = new ArrayList<>(newRanges);
+                }
+                currentRanges.addAll(offsetRanges);
+            }
+            
+            final var currentMin = currentRanges.stream().mapToLong(Range::start).min().orElseThrow();
+            min = Math.min(min, currentMin);
+        }
+        
+        return min;
+    }
+    
+    private static ArrayList<ArrayList<AlmanacMap>> parseAlmanac(final List<String> lines) {
+        final var almanac        = new ArrayList<ArrayList<AlmanacMap>>();
+        var       currentListMap = new ArrayList<AlmanacMap>();
+        almanac.add(currentListMap);
+        
+        for (int i = 3; i < lines.size(); i++) {
+            if (lines.get(i).isBlank()) {
+                currentListMap = new ArrayList<>();
+                almanac.add(currentListMap);
+                i++; // Skip the next "abc-to-xyz:" line
+                continue;
+            }
+            final var parts = Arrays.stream(lines.get(i).split(" ")).mapToLong(Long::parseLong).toArray();
+            currentListMap.add(new AlmanacMap(parts[0], parts[1], parts[2]));
+        }
+        
+        return almanac;
+    }
+    
+    private record AlmanacMap(long dst, long src, long range) {
+        long end() {
+            return src + range;
+        }
+        
+        long offset() {
+            return dst - src;
+        }
+    }
+    
+    private record Range(long start, long end) {
+    }
+}
