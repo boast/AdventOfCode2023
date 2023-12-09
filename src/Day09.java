c3d6e4bb014423a67c75795b195fb1d4db2a596c
Dave Thalmann <david.thalmann@gmail.com>
2023-12-09 10:32:48 +0100

    Day09
    A good day to play around with the Java-Stream-API. As usual, the first step was to parse the input, today in simple list of longs. We then had to create a "repeat finding the difference until all values are 0" function, which - when not implemented too naively - can be a brute force implementation for part 1 and part 2. One thing to look out for is the termination check (all numbers 0) and doing that efficiently (i.e. using `anyMatch(value->value!=0)` is fast and stops at the first non 0 value).
    
    For part 1 we then just sum up the last values of that list of differences.
    
    For part 2 we have to be a bit clever, first we need the first elements and in the right order (note that they are added in reverse order in the `differencesUntilZeroes` method, because in part 1 it does not matter and part 2 we save one reversal of the stream/list). And then we just "subtracting the running total from each value", a.k.a. a reduce with a subtraction starting at 0.
    
    Not too complicated today, only about 13 minutes for part 2.

---
+++ src/Day09.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Day09 {
+    public static void main(final String[] args) throws IOException {
+        final var lines = Files.readAllLines(Paths.get("resources/day09.txt"));
+        
+        System.out.println(part1(lines));
+        System.out.println(part2(lines));
+    }
+    
+    static long part1(final List<String> lines) {
+        final var measurements = parseMeasurements(lines);
+        var       total        = 0L;
+        
+        for (final var measurement : measurements) {
+            final var lastElements = differencesUntilZeroes(measurement).stream().map(List::getLast);
+            total += lastElements.reduce(0L, Long::sum);
+        }
+        
+        return total;
+    }
+    
+    static long part2(final List<String> lines) {
+        final var measurements = parseMeasurements(lines);
+        var       total        = 0L;
+        
+        for (final var measurement : measurements) {
+            final var firstElements = differencesUntilZeroes(measurement).stream().map(List::getFirst);
+            total += firstElements.reduce(0L, (a, b) -> b - a);
+        }
+        
+        return total;
+    }
+    
+    private static List<List<Long>> parseMeasurements(final List<String> lines) {
+        return lines.stream()
+                    .map(line -> line.split(" "))
+                    .map(parts -> Arrays.stream(parts).map(Long::parseLong).toList())
+                    .toList();
+    }
+    
+    private static ArrayList<List<Long>> differencesUntilZeroes(final List<Long> measurement) {
+        final var differenceList = new ArrayList<List<Long>>();
+        differenceList.add(measurement);
+        
+        while (differenceList.getFirst().stream().anyMatch(value -> value != 0L)) {
+            differenceList.addFirst(getDifferences(differenceList.getFirst()));
+        }
+        
+        return differenceList;
+    }
+    
+    private static List<Long> getDifferences(final List<Long> measurement) {
+        final var previous = new AtomicLong(measurement.getFirst());
+        return measurement.stream().skip(1).map(value -> value - previous.getAndSet(value)).toList();
+    }
+}
