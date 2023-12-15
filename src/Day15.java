9492d03c8e8fb45fe96d8661f95909ff6f3eb7f5
Dave Thalmann <david.thalmann@gmail.com>
2023-12-15 11:08:08 +0100

    Day15 Part 1 was very straight forward, just parse and use the char values of the string. Part 2 then was not too hard, but the instructions were obviously heavily over-complicated. But in the end, it is just application of collection types. All in all a rather easy day 15, which makes me scared for the days to come...

---
+++ src/Day15.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
+public class Day15 {
+    
+    public static void main(final String[] args) throws IOException {
+        final var lines = Files.readAllLines(Paths.get("resources/day15.txt"));
+        
+        System.out.println(part1(lines.getFirst()));
+        System.out.println(part2(lines.getFirst()));
+    }
+    
+    static long part1(final String line) {
+        return Arrays.stream(line.split(",")).map(Day15::hash).reduce(0, Integer::sum);
+    }
+    
+    static long part2(final String line) {
+        final var boxes = new ArrayList<LinkedHashMap<String, Integer>>(256);
+        for (int i = 0; i < 256; i++) {
+            boxes.add(new LinkedHashMap<>());
+        }
+        
+        for (final var task : line.split(",")) {
+            final var parts = task.split("[-=]");
+            final var label = parts[0];
+            final var box   = boxes.get(hash(label));
+            
+            if (parts.length == 1) {
+                box.remove(label);
+                continue;
+            }
+            
+            box.put(label, Integer.parseInt(parts[1]));
+        }
+        
+        var sum = 0;
+        for (var box = 0; box < boxes.size(); box++) {
+            var slot = 1;
+            for (final var focalLength : boxes.get(box).values()) {
+                sum += (box + 1) * slot * focalLength;
+                slot++;
+            }
+        }
+        return sum;
+    }
+    
+    private static int hash(final String value) {
+        return value.chars().reduce(0, (a, b) -> ((a + b) * 17) % 256);
+    }
+}
